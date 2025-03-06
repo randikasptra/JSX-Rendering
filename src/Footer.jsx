44f449c9b40f2b6ee8c7d448734cb8d9101b217f
randikasptra <saputrarandika857@gmail.com>
2025-03-06 21:05:31 +0700

    Menambahkan file footer

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Footer() {
+
+    const Angka = 1 ;
+    return (
+        <>
+        <div className="">
+            <h4>Ini adalah Footer</h4>
+            <h4>Ini adalah hasil dari Variabel {Angka}</h4>
+        </div>
+        </>
+    )
+}
